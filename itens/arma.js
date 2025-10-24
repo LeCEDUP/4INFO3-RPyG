746fde926eccb22dde54c034aaf2a23064e8728b
Jeovana <jeovana.klein123@gmail.com>
2025-10-24 20:35:19 -0300

    Criando a Estrutura da Arma

--- itens/arma.js
+++ itens/arma.js
@@ -1,1 +1,11 @@
 // Desenvolva aqui a classe Arma em JS
+const Item = require('./item');
+
+class arma extends Item {
+    constructor( nome, descricao, bonus_ataque) {
+        super(nome, descricao);
+        this.bonus_ataque = bonus_ataque;
+    }   
+}
+
+module.exports = arma;
