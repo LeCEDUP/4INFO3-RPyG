a754cf8e68ffea0543f58b9a9d85c0be8be0d8d0
Jeovana <jeovana.klein123@gmail.com>
2025-10-23 21:43:40 -0300

    Criando a Estrutura do Monstro

--- personagens/monstro.js
+++ personagens/monstro.js
@@ -1,1 +1,11 @@
 // Desenvolva aqui a classe Monstro em JS
+const Personagem = require('./personagem');
+
+class Monstro extends Personagem {
+    constructor(nome, ataque, defesa, vida, tipo) {
+        super(nome, ataque, defesa, vida);
+        this.tipo = tipo;
+    }
+}
+
+module.exports = Monstro;
