97a763febb51d63f15e9500e4002642a0ed9d074
Jeovana <jeovana.klein123@gmail.com>
2025-10-24 20:34:55 -0300

    Criando a Estrutura da Armadura

--- itens/armadura.js
+++ itens/armadura.js
@@ -1,1 +1,11 @@
 // Desenvolva aqui a classe Armadura em JS
+const Item = require('./item');
+
+class Armadura extends Item {   
+    constructor(nome, descricao, bonus_defesa) {
+        super(nome, descricao);
+        this.bonus_defesa = bonus_defesa;
+    }
+}
+
+module.exports = Armadura;
