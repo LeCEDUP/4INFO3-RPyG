a578140393099ce10a5cbc3fa56903ba204ed153
caioonofre <protectcaio@gmail.com>
2025-10-24 14:31:42 -0300

    add: fazendo os testes de personagem

--- teste.js
+++ teste.js
@@ -1,1 +1,8 @@
-// Desenvolva aqui o teste das classes em JS
+import { Personagem } from "./personagens/personagem";
+import { Monstro } from "./personagens/monstro";
+
+const arqueiro = new Personagem("arqueiro", 10, 50, 10);
+const cavaleiro = new Personagem("cavaleiro", 100, 30, 100);
+const dragao = new Monstro("dragao", 1000, 100, 500, "monstro")
+
+arqueiro.atacar(cavaleiro);
