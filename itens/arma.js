113271fd2a5912c4617d2ed1671e46682118e062
caioonofre <protectcaio@gmail.com>
2025-10-24 20:42:05 -0300

    add: classse arma e armadura feita

--- itens/arma.js
+++ itens/arma.js
@@ -1,1 +1,8 @@
-// Desenvolva aqui a classe Arma em JS
+import { Item } from "./item";
+
+class Arma extends Item {
+    constructor(nome, descricao, bonus_ataque) {
+        super(nome, descricao);
+        this.bonus_ataque = bonus_ataque;
+    }
+}
--- itens/armadura.js
+++ itens/armadura.js
@@ -1,1 +1,8 @@
-// Desenvolva aqui a classe Armadura em JS
+import { Item } from "./item";
+
+class Armadura extends Item{
+    constructor(nome, descricao, bonus_defesa) {
+        super(nome, descricao);
+        this.bonus_defesa = bonus_defesa;
+    }
+}
