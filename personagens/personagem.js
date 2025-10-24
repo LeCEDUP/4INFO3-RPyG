cd133f454a57e125f73dd1f45549325c2c2c6ed8
pedroAugustoRo <pedroaugustorodrisilva.br@gmail.com>
2025-10-23 21:56:54 -0300

    traduziindo a classe personagem, fiz personagem e o alvo e terminando o dano

--- personagens/personagem.js
+++ personagens/personagem.js
@@ -1,1 +1,41 @@
-// Desenvolva aqui a classe Personagem em JS
+// // Desenvolva aqui a classe Personagem em JS
+// class Personagem:
+//     def __init__(self, nome, vida, ataque, defesa):
+//         self.nome = nome
+//         self.vida = vida
+//         self.ataque = ataque
+//         self.defesa = defesa
+
+
+class Personagem {
+    constructor(nome, vida, ataque, defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+    }
+
+    
+    // def atacar(self, alvo):
+    //     dano = max(0, self.ataque - alvo.defesa)
+    //     alvo.receber_dano(dano)
+    //     print(f"{self.nome} atacou {alvo.nome} causando {dano} de dano.")
+
+    atacar(alvo) {
+        let dano = Math.max(0, this.ataque - alvo.defesa);
+        alvo.receberDano(dano);
+        console.log(`${this.nome} atacou ${alvo.nome} causando ${dano} de dano.`);
+    }
+    
+    // def receber_dano(self, dano):
+    //     self.vida -= dano
+    //     if self.vida <= 0:
+    //         self.vida = 0
+    //         print(f"{self.nome} foi derrotado!")
+    //     else:
+    //         print(f"{self.nome} recebeu {dano} de dano. Vida restante: {self.vida}")
+    receberDano(dano) {
+        this.vida -= dano;
+        if (this.vida <= 0) {
+            this.vida = 0;
+        }console.log
