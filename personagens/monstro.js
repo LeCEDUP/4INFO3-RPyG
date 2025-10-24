ad4631bd2b0328da7f607b8f7808a910c94b0f12
deBokorni <afonssoleandro@gmail.com>
2025-10-23 21:51:50 -0300

    Começo

--- personagens/monstro.js
+++ personagens/monstro.js
@@ -1,1 +1,16 @@
 // Desenvolva aqui a classe Monstro em JS
+
+import { Personagem } from './personagem.js'
+
+// class Monstro(Personagem):
+//     def __init__(self, nome, vida, ataque, defesa, tipo):
+//         super().__init__(nome, vida, ataque, defesa)
+//         self.tipo = tipo
+
+
+
+class Monstro(personagem){
+    constructor(nome, vida, ataque, defesa, tipo){
+
+    }
+}
--- personagens/personagem.js
+++ personagens/personagem.js
@@ -1,1 +1,57 @@
 // Desenvolva aqui a classe Personagem em JS
+
+// class Personagem:
+    // def __init__(self, nome, vida, ataque, defesa):
+    //     self.nome = nome
+    //     self.vida = vida
+    //     self.ataque = ataque
+    //     self.defesa = defesa
+
+    // def atacar(self, alvo):
+    //     dano = max(0, self.ataque - alvo.defesa)
+    //     alvo.receber_dano(dano)
+    //     print(f"{self.nome} atacou {alvo.nome} causando {dano} de dano.")
+
+    // def receber_dano(self, dano):
+    //     self.vida -= dano
+    //     if self.vida <= 0:
+    //         self.vida = 0
+    //         print(f"{self.nome} foi derrotado!")
+    //     else:
+    //         print(f"{self.nome} recebeu {dano} de dano. Vida restante: {self.vida}")
+
+    // def esta_vivo(self):
+    //     return self.vida > 0
+
+
+class Personagem{
+    constructor(nome, vida, ataque, defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa
+    }
+
+    receber_dano(dano){
+        this.vida -= dano
+        if (this.vida <= 0){
+            this.vida = 0
+            console.log(`{this.nome} foi derrotado!`)}
+        else{
+            console.log(`{this.nome} recebeu {dano} de dano. Vida restante: {self.vida}`)}
+
+
+    }
+
+    atacar(alvo){
+        let dano = Math.max(0, (this.ataque - this.defesa))
+        alvo.receber_dano(dano)
+        console.log(`${self.nome} atacou ${alvo.nome}, causando ${dano} de dano.`)
+
+    }
+        esta_vivo(){
+        return this.vida > 0
+    }
+        
+}
+
