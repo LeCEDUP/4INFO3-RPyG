ca14109b0d460d70838bf57b3da7482d7ae55ddb
Jeovana <jeovana.klein123@gmail.com>
2025-10-24 20:34:39 -0300

    Criando a Estrutura do Item

--- itens/item.js
+++ itens/item.js
@@ -1,1 +1,10 @@
 // Desenvolva aqui a classe Item em JS
+
+class Item {
+    constructor(nome, descricao) {
+        this.nome = nome;   
+       this.descricao = descricao;
+    }
+}
+
+module.exports = Item;
