0f14c26f68e23085e3e90164e37d2268ef36dbd4
pedroAugustoRo <pedroaugustorodrisilva.br@gmail.com>
2025-10-24 20:39:55 -0300

    fiz o import de eprsonagem para a tabela monstros, e terminei o essa tabela👍

--- personagens/monstro.js
+++ personagens/monstro.js
@@ -1,1 +1,22 @@
 // Desenvolva aqui a classe Monstro em JS
+
+// from .personagem import Personagem
+
+// class Monstro(Personagem):
+//     def __init__(self, nome, vida, ataque, defesa, tipo):
+//         super().__init__(nome, vida, ataque, defesa)
+//         self.tipo = tipo
+
+import { Personagem } from './personagem.js';
+
+export class Monstro extends Personagem {
+    constructor(nome, vida, ataque, defesa, tipo) {
+        super(nome, vida, ataque, defesa);
+        this.tipo = tipo;
+    }
+}
+
+// class Monstro(Personagem):
+//     def __init__(self, nome, vida, ataque, defesa, tipo):
+//         super().__init__(nome, vida, ataque, defesa)
+//         self.tipo = tipo
