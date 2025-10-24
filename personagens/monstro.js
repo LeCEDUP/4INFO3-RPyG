d4371718c946be25c763e8790f1eb07cf9df5b4e
caioonofre <protectcaio@gmail.com>
2025-10-24 14:30:55 -0300

    add: classe Monstro feita porem com erro

--- personagens/monstro.js
+++ personagens/monstro.js
@@ -1,1 +1,8 @@
-// Desenvolva aqui a classe Monstro em JS
+import { Personagem } from "./personagem";
+
+export class Monstro extends Personagem {
+    constructor(nome, vida, ataque, defesa, tipo) {
+        super().this(nome,vida, ataque, defesa);
+        this.tipo = tipo;
+    }
+}
