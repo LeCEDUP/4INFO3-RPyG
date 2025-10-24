351fbab7f287a8b67b3b5e5d130d5fec5df0ee34
Alicite <lunacarter1503@gmail.com>
2025-10-23 22:01:39 -0300

    Testando Personagem

--- teste.js
+++ teste.js
@@ -1,1 +1,10 @@
 // Desenvolva aqui o teste das classes em JS
+import { Personagem } from './personagens/personagem.js'
+
+const leticia = new Personagem('Leticia', 75, 25, 5);
+console.log(leticia);
+leticia.receberDano(50);
+console.log(leticia.estaVivo())
+console.log(leticia);
+leticia.receberDano(50);
+console.log(leticia.estaVivo())
