6afde9478ae8518695f84f22295d00074296322d
gogs-sys <gogsh086@gmail.com>
2025-10-23 21:55:55 -0300

    personagem

--- personagens/personagem.js
+++ personagens/personagem.js
@@ -1,1 +1,15 @@
 // Desenvolva aqui a classe Personagem em JS
+class personagem{
+    constructor(nome, vida, ataque, defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+    }
+
+    atacar(alvo) {
+        let dano = Math.max(0,this.ataque - alvo.defesa)
+        alvo.receberDano(dano)
+        console.log(${this})
+    }
+}
