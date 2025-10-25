5b2f84902a36fd7a92e20a05af014347f8c43c2e
caioonofre <protectcaio@gmail.com>
2025-10-24 21:03:38 -0300

    exportando arma e armadura

--- itens/arma.js
+++ itens/arma.js
@@ -1,6 +1,6 @@
 import { Item } from "./item";
 
-class Arma extends Item {
+export class Arma extends Item {
     constructor(nome, descricao, bonus_ataque) {
         super(nome, descricao);
         this.bonus_ataque = bonus_ataque;
--- itens/armadura.js
+++ itens/armadura.js
@@ -1,6 +1,6 @@
 import { Item } from "./item";
 
-class Armadura extends Item{
+export class Armadura extends Item{
     constructor(nome, descricao, bonus_defesa) {
         super(nome, descricao);
         this.bonus_defesa = bonus_defesa;
