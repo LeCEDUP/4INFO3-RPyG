f0f39f28e85a0b140e40d3b7e04410ceda5367ca
pedroAugustoRo <pedroaugustorodrisilva.br@gmail.com>
2025-10-24 21:28:48 -0300

    traduzi a pagina de items e fiz um export para poder associar as outras tabelas

--- itens/item.js
+++ itens/item.js
@@ -1,1 +1,13 @@
 // Desenvolva aqui a classe Item em JS
+
+// class Item:
+//     def __init__(self, nome, descricao):
+//         self.nome = nome
+//         self.descricao = descricao
+
+export class Item {
+    constructor(nome, descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    } 
+}
