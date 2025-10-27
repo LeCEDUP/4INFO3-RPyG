747104de01085ac85c0fa35fc7c02ca6a561460a
miguelkik <miguelaparecidoribeiro06@gmail.com>
2025-10-27 21:23:00 +0000

    classe armadura feita

--- itens/armadura.js
+++ itens/armadura.js
@@ -1,1 +1,10 @@
 // Desenvolva aqui a classe Armadura em JS
+import { Item } from './Item.js';
+
+class Armadura extends Item {
+    constructor (nome, descricao, bonus_defesa){
+        super(nome, descricao);
+
+        this.bonus_defesa = bonus_defesa;
+    }
+}
