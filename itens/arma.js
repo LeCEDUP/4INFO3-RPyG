abdda7d52f003c203d99ada93089eb0f505d935b
pedroAugustoRo <pedroaugustorodrisilva.br@gmail.com>
2025-10-28 20:29:29 -0300

    fiz a classe armas 🪓🔫

--- itens/arma.js
+++ itens/arma.js
@@ -1,1 +1,17 @@
 // Desenvolva aqui a classe Arma em JS
+
+// from .item import Item
+
+// class Arma(Item):
+//     def __init__(self, nome, descricao, bonus_ataque):
+//         super().__init__(nome, descricao)
+//         self.bonus_ataque = bonus_ataque
+
+import Item from './item.js';
+
+export class Arma extends Item {
+    constructor(nome, descricao, bonus_ataque) {
+        super(nome, descricao);
+        this.bonus_ataque = bonus_ataque;
+    }
+}
