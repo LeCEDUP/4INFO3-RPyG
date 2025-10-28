cb612e07375f722417a6cedb1e9ecff2c7cbb3e4
miguelkik <miguelaparecidoribeiro06@gmail.com>
2025-10-28 21:11:34 +0000

    monstro feito

--- personagens/monstro.js
+++ personagens/monstro.js
@@ -1,1 +1,8 @@
 // Desenvolva aqui a classe Monstro em JS
+class Monstro extends Personagem {
+    constructor(nome, vida, ataque, defesa, tipo) {
+        super(nome, vida, ataque, defesa);
+        
+        this.tipo = tipo;
+    }
+}
