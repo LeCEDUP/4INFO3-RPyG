0f365c1881719bd49a4c1f43c2da8af03ac1f4c5
Idenilson27 <voigtidenilson@gmail.com>
2025-10-28 20:44:49 -0300

    armajs

--- itens/arma.js
+++ itens/arma.js
@@ -1,1 +1,6 @@
-// Desenvolva aqui a classe Arma em JS
+from .item import Item
+
+class Arma(Item):
+    def __init__(self, nome, descricao, bonus_ataque):
+        super().__init__(nome, descricao)
+        self.bonus_ataque = bonus_ataque
