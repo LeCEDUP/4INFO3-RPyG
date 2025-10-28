0da245e9555325ae36aff0219c1a1c60ef8261da
miguelkik <miguelaparecidoribeiro06@gmail.com>
2025-10-28 21:27:29 +0000

    aa

--- personagens/heroi.js
+++ personagens/heroi.js
@@ -1,1 +1,10 @@
 // Desenvolva aqui a classe Heroi em JS
+class Heroi extends Personagem {
+    constructor(nome, vida, ataque, defesa, nivel = 1, experiencia = 0, inventario = []) {
+        // Chama o construtor da classe pai (Personagem)
+        super(nome, vida, ataque, defesa);
+        
+        this.nivel = nivel;
+        this.experiencia = experiencia;
+        this.inventario = inventario; 
+    }
