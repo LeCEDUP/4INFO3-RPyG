326b61d287bb2cbebd083611378fb777fac10377
pedroAugustoRo <pedroaugustorodrisilva.br@gmail.com>
2025-10-28 20:15:01 -0300

    criei a classe armadura com  de './item.js' importando

--- itens/armadura.js
+++ itens/armadura.js
@@ -1,1 +1,18 @@
 // Desenvolva aqui a classe Armadura em JS
+
+// from .item import Item
+
+// class Armadura(Item):
+//     def __init__(self, nome, descricao, bonus_defesa):
+//         super().__init__(nome, descricao)
+//         self.bonus_defesa = bonus_defesa
+
+
+import Item from './item.js';
+
+class Armadura extends Item {
+    constructor(nome, descricao, bonus_defesa) {
+        super(nome, descricao);
+        this.bonus_defesa = bonus_defesa;
+    }
+}
