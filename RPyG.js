a303b657d20d510968ed9c58cff5bf45e11844a1
Walmir1974 <walmir.vmartins@gmail.com>
2025-10-28 00:02:34 +0000

    Alteracao para criar menu

--- RPyG.js
+++ RPyG.js
@@ -1,1 +1,8 @@
-// Desenvolva aqui o menu para interagir com o usuário em JS
+import * as readline from 'readline-sync';
+
+import { Item } from './itens/item.js';
+import { Arma } from './itens/arma.js';
+import { Armadura } from './itens/armadura.js';
+import { Heroi } from './personagens/heroi.js';
+import { Monstro } from './personagens/monstro.js';
+
