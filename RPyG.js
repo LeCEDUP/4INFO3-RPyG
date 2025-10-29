82ff053e3e6a40b27b4e376a2906ed5df8f85ab6
gogs-sys <gogsh086@gmail.com>
2025-10-28 21:40:30 -0300

    lutas

--- RPyG.js
+++ RPyG.js
@@ -1,7 +1,7 @@
 import * as readline from 'readline-sync';
 import { Heroi } from './personagens/heroi.js';
 import {Arma} from './itens/arma.js'
-import {Goblin} from './personagens/monstro.js'
+import {Monstro} from './personagens/monstro.js'
 
 const nomeDoHeroi = readline.question('Insira um nome para o seu herói: ')
 console.log(`O nome do seu herói é ${nomeDoHeroi}`)
@@ -12,4 +12,5 @@ console.log(meuHeroi)
 const receberEspada = new Arma('Espada velha','Recebeu Uma Espada longa de uma arvore suspeita', 5)
 console.log(receberEspada)
 
-const lutaGoblin = new Monstro
+const lutaGoblin = new Monstro('Goblin', 100, 10, 20, 'Normal')
+console.log(lutaGoblin)
