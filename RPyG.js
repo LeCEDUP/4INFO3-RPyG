8a902e79c6cfe39b434a12f537c78a2e73565f24
miguelkik <miguelaparecidoribeiro06@gmail.com>
2025-11-03 01:42:24 +0000

    fazendo RpyG

--- RPyG.js
+++ RPyG.js
@@ -1,2 +1,13 @@
 // Desenvolva aqui o menu para interagir com o usuário em JS
-import * as readline from 'readline-sync'
+import { Heroi } from "./personagens/heroi.js";
+import { Arma } from "./itens/arma.js";
+import { Armadura } from "./itens/armadura.js";
+import { Monstro } from "./personagens/monstro.js";
+
+let heroi = new Heroi("Migs", 100, 15, 5);
+let goblin = new Monstro("Gomes Aterrorizante", 130, 8, 2, "Grande");
+let dragao = new Monstro("Pedro Machadinho", 30, 30, 10, "Pequeno");
+
+let espada = new Arma("Dark Blade", "Uma lamina negra misteriosa.", 10);
+let escudo = new Armadura("Shield Hero", "Um escudo resistente o suficiente.", 7);
+let pocaoVida = new Item("Flask of Crimson Tears", "Restaura 30 de vida.");
